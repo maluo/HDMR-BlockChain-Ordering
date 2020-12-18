563e3c0ec17ac5505c1e39a2261b3a7f61e8a101
ma luo <james.luo.3131@gmail.com>
2020-12-17 23:21:20 -0500

    intial commit

---
+++ scripts/test.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2020-12-18 04:39:07
+-- 服务器版本： 10.4.6-MariaDB
+-- PHP 版本： 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `items`
+--
+
+CREATE TABLE `items` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `comments` varchar(1000) DEFAULT NULL,
+  `sys_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `order_num` varchar(255) DEFAULT NULL,
+  `item_num` int(11) DEFAULT NULL,
+  `unit_price` float DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `balance` float DEFAULT NULL,
+  `shipping` float DEFAULT 0,
+  `sales_price` float DEFAULT NULL,
+  `subtotal` float DEFAULT NULL,
+  `gross_income` float DEFAULT NULL,
+  `transaction_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `sys_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- 表的索引 `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_Order_Item` (`item_num`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `items`
+--
+ALTER TABLE `items`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_Order_Item` FOREIGN KEY (`item_num`) REFERENCES `items` (`product_id`) ON DELETE SET NULL ON UPDATE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
